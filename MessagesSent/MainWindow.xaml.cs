9a7978a96c536e634825cb57ceb9525306d3e963
andreasevansdurhamcollege <andreas.evansadamecz@dcmail.ca>
2024-10-23 18:56:39 -0400

    Finished XAML and enter click button. Resolved nested debug issue

--- MessagesSent/MainWindow.xaml.cs
+++ MessagesSent/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace MessagesSent
 {
@@ -16,9 +7,43 @@ namespace MessagesSent
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int day = 1;
+        private int totalMessages = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            int messages;
+            if (int.TryParse(MessagesInput.Text, out messages) && messages >= 0 && messages <= 10000)
+            {
+                totalMessages += messages;
+                MessagesDisplay.Text += $"Day {day}: {messages} messages\n";
+                day++;
+
+                if (day > 7)
+                {
+                    double average = totalMessages / 7.0;
+                    AverageLabel.Content = $"Average: {average:F1} messages";
+                    MessagesInput.IsEnabled = false;
+                    EnterButton.IsEnabled = false;
+                }
+                else
+                {
+                    DayLabel.Content = $"Day {day}";
+                    MessagesInput.Clear();
+                    MessagesInput.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter a number between 0 and 10000");
+                MessagesInput.Focus();
+            }
+        } 
+
+    } 
 }
